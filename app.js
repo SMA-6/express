d46cfc0844b8b74633b30fbbedbcdd2649ba970c
HANA-PON <osigotosagasi2@gamil.com>
2023-12-01 00:59:14 +0900

    Add mongodb access

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var app = express();
 
-// view engine setup
+// view engine setups
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
